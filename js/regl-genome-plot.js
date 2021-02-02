0aa03eef5e27e1e7d7fc75377fa3d7e0cd07da6e
C.Xanthopoulakis <c.xanthopoulakis@gmail.com>
2021-02-03 00:42:38 +0100

    updated to have connections as bezier curves in glsl

--- js/regl-genome-plot.js
+++ js/regl-genome-plot.js
@@ -16,7 +16,6 @@ class ReglGenomePlot {
       attributes: { antialias: true, depth: false, alpha: true},
     });
     this.reglInstance = regl;
-    //this.positions = [[0.0, 0.0], [-0.5, 0.5], [0.5,0.5], [0.5, -0.5], [-0.5, -0.5],[-0.5, 0.5]];
     this.positions = [[0.5, 0.0], [0.0, 0.5], [1.0,0.5], [1.0, -0.5], [0.0, -0.5],[0.0, 0.5]];
     this.rectangleHeight = 10.0;
     this.strokeWidth = 0.66;
@@ -114,6 +113,117 @@ class ReglGenomePlot {
         count: this.positions.length,
         instances: regl.prop('instances')
       });
+      this.connectionSampling = 50; 
+      this.drawConnections = regl({
+          frag: `
+          precision highp float;
+          varying vec4 vColor;
+          void main() {
+            gl_FragColor = vColor;
+          }`,
+
+          vert: `
+          precision highp float;
+          attribute vec2 position;
+          attribute float color;
+          attribute vec2 startPlace, endPlace;
+          uniform vec2 domainX, domainY;
+          varying vec4 vColor;
+          uniform float stageWidth, stageHeight;
+
+          vec2 normalizeCoords(vec2 position) {
+            // read in the positions into x and y vars
+            float x = position[0];
+            float y = position[1];
+
+            return vec2(
+              2.0 * ((x / stageWidth) - 0.5),
+              -(2.0 * ((y / stageHeight) - 0.5)));
+          }
+
+          vec2 bezier(vec2 A, vec2 B, vec2 C, vec2 D, vec2 E, float t) {
+            vec2 A1 = mix(A, B, t);
+            vec2 B1 = mix(B, C, t);
+            vec2 C1 = mix(C, D, t);
+            vec2 D1 = mix(D, E, t);
+
+            vec2 A2 = mix(A1, B1, t);
+            vec2 B2 = mix(B1, C1, t);
+            vec2 C2 = mix(C1, D1, t);
+
+            vec2 A3 = mix(A2, B2, t);
+            vec2 B3 = mix(B2, C2, t);
+  
+            vec2 P = mix(A3, B3, t);
+
+            return P;
+          }
+
+          void main() {
+            float kx = stageWidth / (domainX.y - domainX.x);
+            float ky = -stageHeight / (domainY.y - domainY.x);
+
+            vec2 pointA = vec2(kx * (startPlace.x - domainX.x), stageHeight + ky * (startPlace.y - domainY.x));
+            vec2 pointE = vec2(kx * (endPlace.x - domainX.x), stageHeight + ky * (endPlace.y - domainY.x));
+
+            vec2 pointB = vec2(pointA.x + 1.0, pointA.y);
+            vec2 pointD = vec2(pointE.x - 1.0, pointE.y);
+
+            vec2 pointC = vec2(0.5 * (pointA.x + pointE.x), 0.5 * (pointA.y + pointE.y));
+            if (startPlace.y == endPlace.y) {
+              pointC = vec2(0.5 * (pointA.x + pointE.x), pointA.y - 100.0);
+            }
+
+            vec2 pos = bezier(pointA, pointB, pointC, pointD, pointE, position.x);
+
+            vec2 v = normalizeCoords(pos);
+
+            gl_Position = vec4(v, 0, 1);
+
+            float red = floor(color / 65536.0);
+            float green = floor((color - red * 65536.0) / 256.0);
+            float blue = color - red * 65536.0 - green * 256.0;
+            vColor = vec4(red / 255.0, green / 255.0, blue / 255.0, 1.0);
+          }`,
+
+          attributes: {
+            position: (new Array(this.connectionSampling + 1)).fill().map((x, i) => {
+              return [ i / this.connectionSampling, 0 ]
+            }),
+
+            startPlace: {
+              buffer: regl.prop("startPlace"),
+              divisor: 1
+            },
+
+            endPlace: {
+              buffer: regl.prop("endPlace"),
+              divisor: 1
+            },
+
+            color: {
+              buffer: regl.prop("color"),
+              divisor: 1
+            },
+
+          },
+
+          primitive: 'line strip',
+
+          depth: {
+            enable: false
+          },
+
+          uniforms: {
+            stageWidth: this.stageWidth,
+            stageHeight: this.stageHeight,
+            domainX: regl.prop("domainX"),
+            domainY: regl.prop("domainY")
+          },
+
+          count: this.connectionSampling,
+          instances: regl.prop('instances')
+        });
   }
 
   clear() {
@@ -124,19 +234,27 @@ class ReglGenomePlot {
     });
   }
 
-  load(startPoints, endPoints, yValues, fills, strokes, domainX, domainY) {
-    const startPoint = this.reglInstance.buffer(startPoints);
-    const endPoint = this.reglInstance.buffer(endPoints);
-    const fill = this.reglInstance.buffer(fills);
-    const stroke = this.reglInstance.buffer(strokes);
-    const valY = this.reglInstance.buffer(yValues);
-    const instances = startPoints.length;
+  load(intervalStruct, connections) {
+    const {intervalsStartPoint, intervalsEndPoint, intervalsY, intervalsFill, intervalsStroke, domainX, domainY} = intervalStruct;
+    const startPoint = this.reglInstance.buffer(intervalsStartPoint);
+    const endPoint = this.reglInstance.buffer(intervalsEndPoint);
+    const fill = this.reglInstance.buffer(intervalsFill);
+    const stroke = this.reglInstance.buffer(intervalsStroke);
+    const valY = this.reglInstance.buffer(intervalsY);
+    const instances = intervalsStartPoint.length;
     let color = stroke;
     let offset = 0;
     this.dataBufferStroke = {startPoint, endPoint, color, offset, valY, domainX, domainY, instances};
     color = fill;
     offset = this.strokeWidth;
     this.dataBufferFill = {startPoint, endPoint, color, offset, valY, domainX, domainY, instances};
+
+    let cons = connections.filter(e => ['REF', 'ALT'].includes(e.type));
+    this.dataBufferConnections = {
+      startPlace: this.reglInstance.buffer(cons.map(e => e.edges[0])), 
+      endPlace: this.reglInstance.buffer(cons.map(e => e.edges[1])), 
+      color: this.reglInstance.buffer(cons.map(e => e.color)), 
+      domainX: domainX, domainY: domainY, instances: cons.length};
 
     let xScale = d3.scaleLinear().domain(domainX).range([0, this.stageWidth]);
     d3.select(this.container.node()).select('canvas').call(
@@ -150,6 +268,7 @@ class ReglGenomePlot {
 
           this.dataBufferStroke.domainX = d3.event.transform.rescaleX(xScale).domain();
           this.dataBufferFill.domainX = d3.event.transform.rescaleX(xScale).domain();
+          this.dataBufferConnections.domainX = d3.event.transform.rescaleX(xScale).domain();
           this.render();
     }));
   }
@@ -157,5 +276,6 @@ class ReglGenomePlot {
   render() {
     this.draw(this.dataBufferStroke);
     this.draw(this.dataBufferFill);
+    this.drawConnections(this.dataBufferConnections);
   }
 }
--- js/script.js
+++ js/script.js
@@ -23,20 +23,70 @@ $(function() {
         return hash;
       }, {});
       console.log(genomeLength)
+      // Intervals Processing
       let intervals = dataInput[1].intervals;
-      let intervalsStartPoint = [], intervalsEndPoint = [], intervalsDomainY = [0,0], intervalsY = [], intervalsFill = [], intervalsStroke = [];
+      let intervalBins = {}, intervalsStartPoint = [], intervalsEndPoint = [], domainY = [0,0], intervalsY = [], domainX = [1, genomeLength],intervalsFill = [], intervalsStroke = [];
       intervals.forEach((d,i) => {
-        intervalsStartPoint.push(chromoBins[`${d.chromosome}`].startPlace + d.startPoint);
-        intervalsEndPoint.push(chromoBins[`${d.chromosome}`].startPlace + d.endPoint);
+        d.startPlace = chromoBins[`${d.chromosome}`].startPlace + d.startPoint;
+        intervalsStartPoint.push(d.startPlace);
+        d.endPlace = chromoBins[`${d.chromosome}`].startPlace + d.endPoint;
+        intervalsEndPoint.push(d.endPlace);
         intervalsY.push(+d.y);
         intervalsFill.push(Object.values(d3.rgb(chromoBins[`${d.chromosome}`].color)));
         intervalsStroke.push(Object.values(d3.rgb(chromoBins[`${d.chromosome}`].color).darker()));
-        intervalsDomainY = [d3.min([intervalsDomainY[0], +d.y]), d3.max([intervalsDomainY[1], +d.y])];
+        domainY = [d3.min([domainY[0], +d.y]), d3.max([domainY[1], +d.y])];
+        intervalBins[d.iid] = d;
       });
+      let intervalStruct = {intervalsStartPoint, intervalsEndPoint, intervalsY, intervalsFill, intervalsStroke, domainX , domainY};
+      // Connections Processing
+      let connections = dataInput[1].connections;
+      let connectionVerticesX = [],connectionVerticesY = [], connectionColors = [], connectionLinks = []; connectionCounter = 0;
+      connections.forEach((d,i) => {
+        let origin = intervalBins[Math.abs(d.source)];
+        let target = intervalBins[Math.abs(d.sink)];
+        d.color = (d.type === 'ALT') ? rgbtoInteger('red') : rgbtoInteger('lightgray');
+        if (origin) {
+          d.sourcePlace = (Math.sign(d.source) > 0) ? origin.endPlace : origin.startPlace;
+          d.sourceY = origin.y;
+        }
+        if (target) {
+          d.sinkPlace = (Math.sign(d.sink) > 0) ? target.endPlace : target.startPlace;
+          d.sinkY = target.y;
+        }
+        d.edges = [[d.sourcePlace, d.sourceY], [d.sinkPlace, d.sinkY]].sort((a,b) => d3.ascending(a[0], b[0]));
+      })
+
+      // rendering part
       d3.select('#heading-container2').html(`Finished loading <b>${intervals.length}</b> intervals at <b>${new Date()}</b>`)
       let rGenomePlot = new ReglGenomePlot('genome-plot-container', stageWidth, stageHeight);
-      rGenomePlot.load(intervalsStartPoint, intervalsEndPoint, intervalsY, intervalsFill, intervalsStroke, [1, genomeLength] , intervalsDomainY);
+      rGenomePlot.load(intervalStruct, connections);
       rGenomePlot.render();
     });
+
+    function rgbtoInteger(color) {
+      rgb = d3.rgb(color);
+      return rgb.r * 65536 + rgb.g * 256 + rgb.b;
+    }
+
+    function bezier(connection) {
+      let controlPoints = [], vertices = [];
+      if (['REF', 'ALT'].includes(connection.type)) {
+        controlPoints.push(connection.edges[0]);
+        controlPoints.push([connection.edges[0][0] + 1, connection.edges[0][1]]);
+        if (connection.edges[0][1] === connection.edges[1][1]) {
+          controlPoints.push([0.5 * (connection.edges[0][0] + connection.edges[1][0]), 1.5 * connection.edges[0][1]]);
+        } else {
+          controlPoints.push([0.5 * (connection.edges[0][0] + connection.edges[1][0]), 0.5 * (connection.edges[0][1] + connection.edges[1][1])]);
+        }
+        controlPoints.push([connection.edges[1][0] - 1, connection.edges[1][1]]);
+        controlPoints.push(connection.edges[1]);
+      }
+      for (t = 0; t <= 1.0; t+= 0.1) {
+        let vx = Math.pow(1 - t, 4) * controlPoints[0][0] + 4 * Math.pow(1 - t, 3) * Math.pow(t, 1) * controlPoints[1][0] + 6 * Math.pow(1 - t, 2) * Math.pow(t, 2) * controlPoints[2][0] + 4 * Math.pow(1 - t, 1) * Math.pow(t, 3) * controlPoints[3][0] + Math.pow(t, 4) * controlPoints[4][0];
+        let vy = Math.pow(1 - t, 4) * controlPoints[0][1] + 4 * Math.pow(1 - t, 3) * Math.pow(t, 1) * controlPoints[1][1] + 6 * Math.pow(1 - t, 2) * Math.pow(t, 2) * controlPoints[2][1] + 4 * Math.pow(1 - t, 1) * Math.pow(t, 3) * controlPoints[3][1] + Math.pow(t, 4) * controlPoints[4][1];
+        vertices.push([vx, vy]);
+      }
+      return vertices;
+    }
 
 });
